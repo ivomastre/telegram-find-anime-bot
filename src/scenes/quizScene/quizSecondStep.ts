8e52016f45771342801ec6df1fcf77a4ad9dd6f6
Ivo Augusto Wanderley de Paiva <40743787+ivomastre@users.noreply.github.com>
2021-09-14 19:51:56 -0300

    chore: added custom name mentions

--- src/scenes/quizScene/quizSecondStep.ts
+++ src/scenes/quizScene/quizSecondStep.ts
@@ -7,19 +7,18 @@ const quizSecondStep = async (ctx: any) => {
   const userTelegramId = ctx.update.callback_query.from.id;
   const userFirstName = ctx.update.callback_query.from.first_name;
 
-  const mention = formatMention(userTelegramId, userFirstName);
   let caption = '';
   let stickerName = '';
 
   if (ctx.callbackQuery.data === (ctx.wizard.state as any).correctAnime) {
     const user = await addToUserScore(userTelegramId, userFirstName);
 
-    caption = `${mention} *answered:* \`\`\`\n${ctx.callbackQuery.data}\`\`\` \n*Correct! ✔️ - New Score: ${user.score}*`;
+    caption = `${formatMention(userTelegramId, user.name)} *answered:* \`\`\`\n${ctx.callbackQuery.data}\`\`\` \n*Correct! ✔️ - New Score: ${user.score}*`;
     stickerName = SUCCESS_STICKER;
   } else {
     const user = await deductToUserScore(userTelegramId, userTelegramId);
 
-    caption = `${mention} *answered:* \`\`\`\n${ctx.callbackQuery.data}\`\`\` \n*Wrong! ❌ - New Score: ${user.score}*`;
+    caption = `${formatMention(userTelegramId, user.name)} *answered:* \`\`\`\n${ctx.callbackQuery.data}\`\`\` \n*Wrong! ❌ - New Score: ${user.score}*`;
     stickerName = FAILURE_STICKER;
   }
 
